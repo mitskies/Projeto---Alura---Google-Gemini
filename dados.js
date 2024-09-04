3cf5ee1621133adde1bc03b31407fd7498f28ad1
Steh <89545847+mitskies@users.noreply.github.com>
2024-09-04 11:58:51 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,79 @@
+let integrantesAespa = [
+    {
+      nomeArtistico: "Karina",
+      nomeCompleto: "Yoo Ji-min",
+      grupo: "aespa",
+      funcao: "Líder, Dançarina Principal",
+      dataNascimento: "11 de Abril de 2000",
+      nacionalidade: "Sul-coreana",
+      habilidades: ["Dança", "Canto", "Rap"],
+      redesSociais: {
+        instagram: "katarinabluu",
+        twitter: "aespa_official",
+        tiktok: "aespa_official"
+      },
+      fatosInteressantes: [
+        "É conhecida por sua dança poderosa e expressiva.",
+        "É fluente em coreano e inglês.",
+        "É embaixadora de diversas marcas."
+      ]
+    },
+    {
+      nomeArtistico: "NingNing",
+      nomeCompleto: "Ning Yizhuo",
+      grupo: "aespa",
+      funcao: "Vocalista Principal",
+      dataNascimento: "23 de outubro de 2002",
+      nacionalidade: "Chinesa",
+      habilidades: ["Canto", "Dança"],
+      redesSociais: {
+        instagram: "imnotningning",
+        twitter: "aespa_official",
+        tiktok: "aespa_official"
+      },
+      fatosInteressantes: [
+        "Conhecida por sua voz poderosa e técnica vocal impecável.",
+        "Participou de diversos programas de talentos na China antes de sua estreia.",
+        "É fluente em mandarim e coreano."
+      ]
+    },
+    {
+      nomeArtistico: "Giselle",
+      nomeCompleto: "Aeri Uchinaga",
+      grupo: "aespa",
+      funcao: "Rapper Principal",
+      dataNascimento: "30 de outubro de 2000",
+      nacionalidade: "Japonesa",
+      habilidades: ["Rap", "Canto", "Dança"],
+      idiomas: ["Japonês", "Coreano", "Inglês"],
+      redesSociais: {
+        instagram: "aerichandesu",
+        twitter: "aespa_official",
+        tiktok: "aespa_official"
+      },
+      fatosInteressantes: [
+        "É fluente em três idiomas: japonês, coreano e inglês.",
+        "Cresceu na Coreia do Sul.",
+        "É conhecida por seus raps rápidos e versáteis."
+      ]
+    },
+    {
+      nomeArtistico: "Winter",
+      nomeCompleto: "Kim Min-jeong",
+      grupo: "aespa",
+      funcao: "Visual, Dançarina Líder",
+      dataNascimento: "1 de janeiro de 2001",
+      nacionalidade: "Sul-coreana",
+      habilidades: ["Dança", "Canto", "Visual"],
+      redesSociais: {
+        instagram: "imwinter",
+        twitter: "aespa_official",
+        tiktok: "aespa_official"
+      },
+      fatosInteressantes: [
+        "Considerada a visual do grupo, com uma beleza única.",
+        "É conhecida por sua dança delicada e elegante.",
+        "É uma grande fã da Minnie do (G)I-DLE."
+      ]
+    }
+  ];
